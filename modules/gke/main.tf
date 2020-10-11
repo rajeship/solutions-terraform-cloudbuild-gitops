13eaad7b83e57e939f249f4358775a5d2139fdf2
rajeship <rajeshkumarip@gmail.com>
2020-10-11 20:45:06 +0530

    Update gke module

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -1,3 +1,17 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = "${var.project}"
@@ -24,7 +38,6 @@ module "gke" {
       image_type         = "COS"
       auto_repair        = true
       auto_upgrade       = true
-      #service_account    = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
       service_account    = "iac-admin@fsi-retailbanking-dev.iam.gserviceaccount.com"
       preemptible        = false
       initial_node_count = 80
