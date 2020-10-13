a90821d9c1e114d63d5593b4dadb2ee18c32b584
rajeship <rajeshkumarip@gmail.com>
2020-10-13 17:48:54 +0530

    updated the network and subnet link for gke cluster

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -50,7 +50,7 @@ module "cluster-1" {
   source = "../../modules/gke-cluster"
   project_id                = "${var.project}"
   name                      = "cluster-1"
-  location                  = "us-central1-c"
+  location                  = "us-west1-b"
   network                   = var.network_self_link
   subnetwork                = var.subnet_self_link
   secondary_range_pods      = "pods"
@@ -73,6 +73,6 @@ module "cluster-1-nodepool-1" {
   source                      = "../../modules/gke-nodepool"
   project_id                  = "${var.project}"
   cluster_name                = "cluster-1"
-  location                    = "us-central1-c"
+  location                    = "us-west1-b"
   name                        = "nodepool-1"
 }
